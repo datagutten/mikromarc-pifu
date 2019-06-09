6144d21d91aadfbec9065222e36c0f5d05f6b996
datagutten <datagutten@datagutten.net>
2019-06-09 09:39:50 +0200

    Fetch all persons on a school including all teachers and staff

--- elever_mikromarc_pifu.php
+++ elever_mikromarc_pifu.php
@@ -24,67 +24,69 @@ foreach($pifu->schools() as $school) //Loop gjennom skoler
 	$skolekey=$school->sourcedid->id;
 	$skole=$school->description->long;
 
-	$groups=$pifu->ordered_groups($school->sourcedid->id);
-	if(empty($groups))
-		continue;
+	$members = $pifu->group_members($school);
+    foreach($members as $member) //Loop gjennom elever i klassen
+    {
+        $person_key=(string)$member->sourcedid->id;
+        $person=$pifu->person($person_key)[0];
+        if(stripos($person->name->n->family,'Test')!==false)
+            continue;
+        if($person->demographics->gender==1)
+            $gender='F';
+        else
+            $gender='M';
+        $role = (string)$member->role->attributes()['roletype'];
 
-	foreach($groups as $group) //Loop gjennom klasser på skolen
-	{
-		$klassekey=(string)$group->comments;
-		//$klassekey=(string)$group->sourcedid->id;
-		$members=$pifu->members($group->sourcedid->id);
-		if(empty($members))
-			continue;
+        if($role!='01')
+        {
+            $gruppe='Ansatte';
+            $class='';
+        }
+        else
+        {
+            $gruppe='Elever';
+            $xpath = sprintf('/enterprise/membership/member/sourcedid/id[.="%s"]/ancestor::membership/sourcedid/id[contains(., "schoolclass")]', $person_key);
+            $group_id = $pifu->xml->xpath($xpath);
+            if(empty($group_id))
+                $class='';
+            else
+            {
+                $group = $pifu->group_info_id((string)$group_id[0]);
+                $class = (string)$group->description->short;
+            }
+        }
 
-		foreach($members as $member) //Loop gjennom elever i klassen
-		{
-			$person=$pifu->person($key=(string)$member->sourcedid->id)[0];
+        if(empty($person->extension->pifu_adr))
+        {
+            $adr=new stdClass();
+            $adr->street='';
+            $adr->pcode='';
+            $adr->locality='';
+        }
+        else
+            $adr=$person->extension->pifu_adr->adr;
+        //"Etternavn","Fornavn","Født","Skole","Klasse","Satsnummer","Personnr","Kjønn","Postadresse","Postnummer","Poststed","Gruppe","Telefon","Mobil","Epost"
+        $fields=array((string)$person->name->n->family, //Etternavn
+                      (string)$person->name->n->given, //Fornavn
+                      date('d.m.y',strtotime($person->demographics->bday)), //Født
+                      (string)$school->description->long, //Skole
+                      $class, //Klasse
+                      (string)$person->userid[1], //GUID
+                      (string)$person->userid[0], //Fødselsnummer
+                      $gender, //Kjønn
+                      (string)$adr->street, //Postadresse
+                      (string)$adr->pcode, //Postnummer
+                      (string)$adr->locality, //Poststed
+                      $gruppe, //Gruppe
+                      $pifu->phone($person,1), //Telefon
+                      $pifu->phone($person,3), //Mobil
+                      (string)$person->email, //Epost
+                     );
+        unset(/*$fields[6],*/$adr);
 
-			if(stripos($person->name->n->family,'Test')!==false)
-				continue;
-			if($person->demographics->gender==1)
-				$gender='F';
-			else
-				$gender='M';
-			/*if($klassekey=='Ansatt')
-			{
-				$gruppe='Ansatte';
-				$klassekey_output='';
-			}
-			else*/
-				$gruppe='Elever';
-			if(empty($person->extension->pifu_adr))
-			{
-				$adr=new stdClass();
-				$adr->street='';
-				$adr->pcode='';
-				$adr->locality='';
-			}
-			else
-				$adr=$person->extension->pifu_adr->adr;
-			//"Etternavn","Fornavn","Født","Skole","Klasse","Satsnummer","Personnr","Kjønn","Postadresse","Postnummer","Poststed","Gruppe","Telefon","Mobil","Epost"
-			$fields=array((string)$person->name->n->family, //Etternavn
-						  (string)$person->name->n->given, //Fornavn
-						  date('d.m.y',strtotime($person->demographics->bday)), //Født
-						  (string)$school->description->long, //Skole
-						  (string)$group->description->short, //Klasse
-						  (string)$person->userid[1], //GUID
-						  (string)$person->userid[0], //Fødselsnummer
-						  $gender, //Kjønn
-						  (string)$adr->street, //Postadresse
-						  (string)$adr->pcode, //Postnummer
-						  (string)$adr->locality, //Poststed
-						  $gruppe, //Gruppe
-						  $pifu->phone($person,1), //Telefon
-						  $pifu->phone($person,3), //Mobil
-						  (string)$person->email, //Epost
-						 );
-			unset(/*$fields[6],*/$adr);
-
-			array_walk($fields,'encodeCSV'); //Bruk encodeCSV på alle feltene
-			fputcsv_eol($fp,$fields,"\r\n"); //Skriv en elev til fil	
-		}
-	}
+        array_walk($fields,'encodeCSV'); //Bruk encodeCSV på alle feltene
+        fputcsv_eol($fp,$fields,"\r\n"); //Skriv en elev til fil
+    }
 }
 fclose($fp); //Lukk filen
 function fputcsv_eol($fp, $array, $eol) {
@@ -93,4 +95,3 @@ function fputcsv_eol($fp, $array, $eol) {
     fwrite($fp, $eol);
   }
 }
-?>
